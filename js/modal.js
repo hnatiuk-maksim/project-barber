a012e04d85be6c469c95c784574ca9c6c9caf58b
hnatiuk-maksim <maximgnat24@gmail.com>
2022-12-18 17:53:21 +0200

    mobile-end

--- js/modal.js
+++ js/modal.js
@@ -1,17 +1,42 @@
-(() => {
-    const refs = {
-      openModalBtn: document.querySelector('[data-modal-open]'),
-      closeModalBtn: document.querySelector('[data-modal-close]'),
-      modal: document.querySelector('[data-modal]'),
-      body: document.querySelector('body'),
-      input: document.querySelector('.modal-input'),
-    };
+// (() => {
+//     const refs = {
+//       openModalBtn: document.querySelector('[data-modal-open]'),
+//       closeModalBtn: document.querySelector('[data-modal-close]'),
+//       modal: document.querySelector('[data-modal]'),
+//       body: document.querySelector('body'),
+//       input: document.querySelector('.modal-input'),
+//     };
   
-    refs.openModalBtn.addEventListener('click', toggleModal);
-    refs.closeModalBtn.addEventListener('click', toggleModal);
+//     refs.openModalBtn.addEventListener('click', toggleModal);
+//     refs.closeModalBtn.addEventListener('click', toggleModal);
   
-    function toggleModal() {
-      refs.modal.classList.toggle('is-hidden');
-      refs.body.classList.toggle('no-scroll');
-    }
-  })();
+//     function toggleModal() {
+//       refs.modal.classList.toggle('is-hidden');
+//       refs.body.classList.toggle('no-scroll');
+//     }
+//   })();
+
+let backdrop = document.querySelector('.backdrop'); //Фон модального вікна
+let modal = document.querySelector('.modal'); //Модальне вікно
+let openPopupButtons = document.querySelectorAll('.open-popup'); //Кнопки для відкриття вікна
+let closePopupButton = document.querySelector('.close-popup');
+
+openPopupButtons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    e.preventDefault();
+    backdrop.classList.add('active');
+    modal.classList.add('active');
+  })
+});
+
+closePopupButton.addEventListener('click', () => {
+  backdrop.classList.remove('active');
+  modal.classList.remove('active');
+});
+
+document.addEventListener('click', (e) => {
+  if(e.target === backdrop) {
+    backdrop.classList.remove('active');
+    modal.classList.remove('active');
+  }
+});
